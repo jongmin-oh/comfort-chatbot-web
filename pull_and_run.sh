7ece89a7702024c452821ecad9f662842c4618bc
j5ng <j5ng>
2023-10-03 17:41:36 +0900

    init

--- pull_and_run.sh
+++ pull_and_run.sh
@@ -2,6 +2,8 @@ if [[ -f .env ]]; then
     export $(cat .env | xargs)
 fi
 
+docker stop ${AWS_ECR_URL} ; docker rm ${AWS_ECR_URL} ; docker rmi ${AWS_ECR_URL}/${AWS_ECR_URL} ; docker rmi ${AWS_ECR_URL}
+
 aws ecr get-login-password --region ap-northeast-2 | docker login --username AWS --password-stdin ${AWS_ECR_URL}
 docker pull ${AWS_ECR_URL}/${SERVER_NAME}:latest
 
