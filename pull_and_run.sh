95e13341058f004d385dc5fbed05a1af7e5eda41
j5ng <j5ng>
2023-10-03 17:28:30 +0900

    init

--- pull_and_run.sh
+++ pull_and_run.sh
@@ -5,5 +5,5 @@ fi
 aws ecr get-login-password --region ap-northeast-2 | docker login --username AWS --password-stdin ${AWS_ECR_URL}
 docker pull ${AWS_ECR_URL}/${SERVER_NAME}:latest
 
-docker tag ${AWS_ECR_URL}/${SERVER_NAME}:latest comfort:latest
+docker tag ${AWS_ECR_URL}/${SERVER_NAME}:latest ${SERVER_NAME}:latest
 docker run --name ${SERVER_NAME} --restart=always --network=bridge -d -p ${PORT}:${PORT} ${SERVER_NAME}:latest
